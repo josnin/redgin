e7908e66e3f1f6d1176a020e00b1770d56dd39f0
josnin <nino.capati@gmail.com>
2022-12-09 19:48:03 +0800

    Create loop-binding.ts

---
+++ loop-binding.ts
@@ -0,0 +1,36 @@
+import { RedGin, bind } from "./red-gin.js";
+
+// can h remove `this` ?
+
+class LoopBinding extends RedGin {
+  arr: any = [1, 2, 3]
+
+  set arr(val) {
+     this.arr = val
+     this.getElementById('withLoop').innerHTML = this.forLoop()
+  }
+ 
+  render() {
+    const { arr, forLoop } = this
+    
+    return `
+        <div id="withLoop">
+          ${ forLoop() }
+        </div> ` 
+  }
+
+  forLoop(){
+    return  `${ this.arr.map( (e: any) => {
+                  return `
+                    <button>
+                        first ${e} button
+                        This is just a test??
+                    </button>`
+                }) }
+           `
+  }
+ 
+}
+
+
+customElements.define('loop-binding', LoopBinding);

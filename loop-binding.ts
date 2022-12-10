14528e28ccd91f8ac4a787abdcdff3e8af0c56ee
josnin <nino.capati@gmail.com>
2022-12-10 21:58:34 +0800

    Update loop-binding.ts

--- loop-binding.ts
+++ loop-binding.ts
@@ -1,29 +1,28 @@
-import { RedGin, bind } from "./red-gin.js";
+import { RedGin, div } from "./red-gin.js";
 
 // can h remove `this` ?
 
 class LoopBinding extends RedGin {
-  #arr: any = [1, 2, 3]
+  //#arr: any = [1, 2, 3]
 
-  set arr(val) {
-     this.#arr = val
-     this.bind('forLoop1')
-     this.shadowRoot.getElementById('forLoop1').innerHTML = this.forLoop1()  // how to auto generate every setter?
-  }
+  //set arr(val) {
+  //   this.#arr = val
+  //   this.bind('forLoop1')
+  //   this.shadowRoot.getElementById('forLoop1').innerHTML = this.forLoop1()  // how to auto generate every setter?
+  //}
  
   render() {
     //const { #arr, forLoop1 } = this
     
     return `
-        <div id="forLoop1">
-          ${ this.forLoop1() }
-        </div> ` 
+       ${ div('arr', this.forLoop()) }
+      `
   }
 
-  forLoop1(){
+  forLoop(){
     // forLoop{n} function html
     // how about forloop inside forloop ??
-    return  `${ this.#arr.map( (e: any) => {
+    return  `${ this.arr.map( (e: any) => {
                   return `
                     <button>
                         first ${e} button

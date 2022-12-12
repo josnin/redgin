4ba25eb04147c0a2d4b4007ae4eba9ede994e1bf
josnin <nino.capati@gmail.com>
2022-12-12 21:47:05 +0800

    Update sample-binding.ts

--- sample-binding.ts
+++ sample-binding.ts
@@ -2,17 +2,16 @@ import { RedGin, div, input } from "./gin.js";
 
 
 class Binding extends RedGin {
-  _title: string = 'Hello world!'
+  title: string = 'Hello world!'
 
   render() {
     return `
           <input ${ input( (e) => changeHandler(e)) } type="text"/>
           
           <!--
-          This will also generate a setter
-          <div id="title1">${this.#title}</div>
+          This will also generate a setter         
           -->
-          ${ div('_title') } 
+          ${ div('title') } 
                
         
     `

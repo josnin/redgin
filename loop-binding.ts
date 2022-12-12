1186add7abbbfdaa6dd2c02f6898db976b230a60
josnin <nino.capati@gmail.com>
2022-12-12 21:29:00 +0800

    Update loop-binding.ts

--- loop-binding.ts
+++ loop-binding.ts
@@ -3,22 +3,20 @@ import { RedGin, div } from "./red-gin.js";
 
 
 class LoopBinding extends RedGin {
-  _arr: any = []
-  arr: any = []
-
-  //set arr(val) {
-  //   this.#arr = val
-  //   this.bind('forLoop1')
-  //   this.shadowRoot.getElementById('forLoop1').innerHTML = this.forLoop1()  // how to auto generate every setter?
-  //}
  
+  arr: any = []
+  
+  static get observedAttributes() {
+    return ['arr'];
+  }
+  
   render() {
        
     return `
         <div>Test Loop Binding<div/>
         <div>
            This will loop thru the array
-            ${ div('_arr', { exp: this.forLoop }) }
+            ${ div('arr', { exp: this.forLoop }) }
          </div>       
       `
   }

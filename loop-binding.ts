321f8250b380b75008a468fdd7b8c6f73cb49ae4
josnin <nino.capati@gmail.com>
2022-12-13 21:23:46 +0800

    Update loop-binding.ts

--- loop-binding.ts
+++ loop-binding.ts
@@ -16,14 +16,9 @@ class LoopBinding extends RedGin {
         <div>
            <!-- re render when value change -->                   
           ${ div('arr', () => this.arr.map( (e: any) => {
-                          return t`
-                            <button>
-                                first ${e} button
-                                This is just a test??
-                            </button>`
-                          
-                        }) 
-          ) }    
+                                  return t`<button>first ${e} button</button>` 
+                 })   
+            )}    
       `
   }
   

1480b8a6ecd84d9cfb0e08a54773245bb86ab91e
josnin <nino.capati@gmail.com>
2022-12-30 17:22:13 +0800

    Update sample-event.ts

--- src/samples/sample-event.ts
+++ src/samples/sample-event.ts
@@ -2,12 +2,12 @@ import { RedGin, click, propReflect, watch } from "../red-gin.js";
 
 
 class Event extends RedGin {
-  arr: any = propReflect([1, 2, 3], { type: Array })
+  arr = propReflect([1, 2, 3], { type: Array })
 
   render() {
     return `
-        ${ watch(['arr'], () => this.arr.map( (e: any) => `
-                    <button ${ click( (e:any) => alert(e) )} >clickMe</button>
+        ${ watch(['arr'], () => this.arr.map( e => `
+                    <button ${ click( () => alert(e) )} >clickMe</button>
                   `) 
         ) }
     `

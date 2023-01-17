3c87fc6bd4479d8c05d0a952677a4982069de03a
Jose <nino.capati@gmail.com>
2023-01-17 22:09:18 +0800

    bootstrap sharedStyle sample

---
+++ src/samples/sharedStyles/bootStrap.ts
@@ -0,0 +1,22 @@
+
+import { RedGin } from "../../redgin.js";
+
+
+class BootStrap extends RedGin {
+
+  render() {       
+    return ` 
+      <button type="button" class="btn">Basic</button>
+      <button type="button" class="btn btn-default">Default</button>
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-link">Link</button>
+      `
+  }
+ 
+}
+
+customElements.define('btn-bootstrap', BootStrap);

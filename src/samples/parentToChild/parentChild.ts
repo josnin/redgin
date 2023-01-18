1f9612075d99371a94b185012b4759a3c340a874
Jose <nino.capati@gmail.com>
2023-01-18 21:31:12 +0800

    sample send data to child comp

---
+++ src/samples/parentToChild/parentChild.ts
@@ -0,0 +1,63 @@
+
+
+import { RedGin, getset, watch, propReflect } from "../../redgin.js";
+
+// configure child component
+class Child extends RedGin {
+
+  item = getset<string>('My item?')
+
+  render() {       
+    return `<p>
+        Today's item: ${ watch(['item'], () => this.item ) }
+    </p>`
+  }
+ 
+}
+
+// configure child component
+class Child2 extends RedGin {
+
+  item = propReflect<string>('My item?')
+
+  static observedAttributes = ['item']
+
+  render() {       
+    return `<p>
+        Today's item: ${ watch(['item'], () => this.item ) }
+    </p>`
+  }
+ 
+}
+
+
+
+class Parent extends RedGin {
+
+  currentItem: string = 'Laptop'
+
+  onInit() {
+    /*
+     * send data to child component using properties
+     */
+    const child = this.shadowRoot?.querySelector('child-comp')
+    // @ts-ignore
+    child.item = this.currentItem
+  }
+
+  render() {       
+    return ` 
+            <child-comp></child-comp>
+
+            <!-- send data to child component using attributes -->
+            <child2-comp item="${this.currentItem}"></child2-comp>
+      `
+  }
+ 
+}
+
+
+
+customElements.define('child-comp', Child);
+customElements.define('child2-comp', Child2);
+customElements.define('parent-comp', Parent);

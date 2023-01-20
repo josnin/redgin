1201b6d03f142cfb777a7557b8c462402c4187d8
Jose <nino.capati@gmail.com>
2023-01-20 14:58:09 +0800

    introduce emit for customEvent

--- src/directives/events.ts
+++ src/directives/events.ts
@@ -1,6 +1,7 @@
 import { getUniqID } from '../utils.js'
 
-var eventRef: any = []
+const eventRef: any = []
+
 enum EventListenType {
   ADD = 0,
   REMOVE = 1,
@@ -16,6 +17,14 @@ export const event = (type: string, fn: any) => {
   return `data-evt__=${uniq}`
 }
 
+/*
+ * emit.call(this, 'newItem', item)
+ */
+export function emit(this: any, customEvent: string, value: any){
+  const event = new CustomEvent(customEvent, { detail: value, composed: true });
+  if (this.shadowRoot) this.shadowRoot.dispatchEvent(event);
+}
+
 
 function baseEvent(this: any, etype: EventListenType) {
     // todo? update only with changes
@@ -23,8 +32,9 @@ function baseEvent(this: any, etype: EventListenType) {
       const [evt, fn, id] = e
       if (this.shadowRoot) {
         let el: HTMLElement = this.shadowRoot.querySelector(`[data-evt__="${id}"]`)
-        //el.addEventListener(evt, fn)
-        etype === EventListenType.ADD ? el.addEventListener(evt, fn) : el.removeEventListener(evt, fn)
+        if (el) { // either to clear eventRef or add this?
+          etype === EventListenType.ADD ? el.addEventListener(evt, fn) : el.removeEventListener(evt, fn)
+        }
       }
     }
 

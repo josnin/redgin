037ba019df5645276c1c318270819c8c17c14248
Jose <nino.capati@gmail.com>
2023-01-23 11:35:48 +0800

    cleanup

--- src/props/props.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function applyPropsBehavior(this: any, prop: string, propValue: string): void;
-export declare class customPropsBehavior {
-    static reg: any;
-    static define(p: any): void;
-}
